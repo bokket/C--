4b635178ae9155667174c256f8534b8f97caf9fc
bokket <3100563328@qq.com>
2020-10-15 20:04:40 +0800

    commit

---
+++ Data/data.cpp
@@ -0,0 +1,14 @@
+#include "data.h"
+void Data::init() {
+    cin>>year;
+    cin>>month;
+    cin>>day;
+}
+void Data::print() {
+    cout<<"year"<<"month"<<"day"<<endl;
+    cout<<year<<mounth<<day<<endl;
+}
+int Data::isLeapYear()
+{
+    if(year)
+}
---
+++ Data/data.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+struct Data
+{
+    //成员函数
+    //函数成员
+public:
+    void init();
+    void print();
+
+private:
+    //数据成员
+    int month;
+    int year;
+    int day;
+}
