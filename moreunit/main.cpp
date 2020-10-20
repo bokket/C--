9515d7d8ae1c963b16d7a6d30102c3c016a0afc1
bokket <3100563328@qq.com>
2020-10-20 18:17:58 +0800

    commit

---
+++ moreunit/main.cpp
@@ -0,0 +1,37 @@
+//
+// Created by bokket on 2020/10/20.
+//
+#include <iostream>
+using namespace std;
+struct Point{
+    int add(int x,int y)
+    {
+        return x+y;
+    }
+    int minus(int x,int y)
+    {
+        return x-y;
+    }
+    int mul(int x,int y)
+    {
+        return x*y;
+    }
+    int div(int x,int y)
+    {
+        return x/y;
+    }
+};
+int oper(Point &p,int(Point::*pf)(int x,int y),int x,int y)
+{
+    return (p.*pf)(x,y);
+}
+
+typedef int (Point::*PF)(int x,int y);
+int main()
+{
+    Point p;
+    PF pf=&Point::add;
+
+    pf=&Point::mul;
+    cout<<oper(p,pf,5,2)<<endl;
+}
